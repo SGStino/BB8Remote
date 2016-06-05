3eba957f765251341611dad25da630bd0f828bac
Stijn Tallon <stijn_tallon@msn.com>
2016-06-05 19:12:31 +0200

    order of the motors

--- BB8Remote/main.cpp
+++ BB8Remote/main.cpp
@@ -2,12 +2,13 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <cmath>
 
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
 
 #include <RF24/RF24.h>
-
 
 #define I2C_ADC_ADDR  0x48
 
@@ -21,13 +22,9 @@ const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 struct RequestPayload
 {
 	const uint16_t header = 0xF00F;
-	char text[10] = { '0','1','2','3','4','5','6','7','8','9' };
-	float rightX;
-	float rightY;
-	float leftX;
-	float leftY;
-	bool left;
-	bool right;
+	uint8_t directions;
+	uint8_t speeds[6];
+	uint8_t reserved;
 	const uint16_t footer = 0x0FF0;
 };
 
@@ -37,16 +34,25 @@ struct ResponsePayload
 	bool status;
 	const uint16_t footer = 0x0FF0;
 };
+float clip(float n, float lower, float upper) {
+	return std::max(lower, std::min(n, upper));
+}
+
+const float scaleX = -1.0f / 2.0f;
+const float scaleY = 1.73205080757f / 2.0f;
 
 int main(void)
 {
 	std::cout << "initializing wiringPi\n";
 	//int wiringHandle = wiringPiSetupSys();
-	//printf("%x \n", wiringHandle);
+	//printf("%x \n", wiringHandle); 
+	std::cout << "init" << bcm2835_init() << "\n";
 
 	std::cout << "setting pin modes\n";
 	//pinMode(0, INPUT);
 	//pinMode(2, INPUT);
+	bcm2835_gpio_fsel(RPI_V2_GPIO_P1_11, BCM2835_GPIO_FSEL_INPT);
+	bcm2835_gpio_fsel(RPI_V2_GPIO_P1_13, BCM2835_GPIO_FSEL_INPT);
 
 	std::cout << "setting pin pullups\n";
 	//pullUpDnControl(0, PUD_UP);
@@ -80,25 +86,68 @@ int main(void)
 		auto inRightY = ads1115->ReadValue(AdcMux::AINP1NGND);
 		auto inLeftX = ads1115->ReadValue(AdcMux::AINP2NGND);
 		auto inLeftY = ads1115->ReadValue(AdcMux::AINP3NGND);
-		requestPayload.rightX = 1 - scale * inRightX;
-		requestPayload.rightY = 1 - scale * inRightY;
-		requestPayload.leftX = 1 - scale * inLeftX;
-		requestPayload.leftY = 1 - scale * inLeftY;
-		requestPayload.left = bcm2835_gpio_lev(RPI_V2_GPIO_P1_11);
-		requestPayload.right = bcm2835_gpio_lev(RPI_V2_GPIO_P1_13);
+
+		
+
+		auto rightX = clip(1 - scale * inRightX, -1, 1);
+		auto rightY = clip(1 - scale * inRightY, -1, 1);
+
+		auto powerRight = std::sqrt(rightX * rightX + rightY * rightY);
+
+		rightX *= powerRight;
+		rightY *= powerRight;
+
+
+		auto leftX = clip(1 - scale * inLeftX, -1, 1);
+		auto leftY = clip(1 - scale * inLeftY, -1, 1);
+
+
+		auto powerLeft = std::sqrt(leftX * leftX + leftY * leftY);
+
+		leftX *= powerLeft;
+		leftY *= powerLeft;
+
+
+		auto left = bcm2835_gpio_lev(RPI_V2_GPIO_P1_11);
+		auto right = bcm2835_gpio_lev(RPI_V2_GPIO_P1_13);
+
+
+		std::cout << "\x1B[2J\x1B[H";
+
+		float rotation = 0;
+		float headRotation = (left ? 1 : 0) + (right ? -1 : 0);
+		float motors[6];
+
+		motors[0] = (scaleX * rightX) - (scaleY * rightY) + rotation;
+		motors[1] = (scaleX * rightX) + (scaleY * rightY) + rotation;
+		motors[2] = rightX + rotation;
+
+		motors[3] = headRotation;
+		motors[4] = leftX;
+		motors[5] = leftY;
+
+		requestPayload.directions = 0; 
+		for (int i = 0; i < 6; i++)
+		{
+			if (motors[i] < 0)
+				requestPayload.directions |= 1 << i;
+			requestPayload.speeds[i] = clip(abs(motors[i] * 255.0f),0,255.0f);
+			printf("%x %f %X\n",i, motors[i], requestPayload.speeds[i]);
+		}
+		printf("%x\n", requestPayload.directions);
+
 
 		radio.stopListening();
 		radio.write(&requestPayload, sizeof(RequestPayload));
 		radio.startListening();
 
 
-		std::cout << "\x1B[2J\x1B[H";
-		printf("1 %f %X\n", requestPayload.rightX, inRightX);
-		printf("1 %f %X\n", requestPayload.rightY, inRightY);
-		printf("2 %f %X\n", requestPayload.leftX, inLeftX);
-		printf("3 %f %X\n", requestPayload.leftY, inLeftY);
-		printf("A %s \n", requestPayload.left ? "Down" : "Up");
-		printf("B %s \n", requestPayload.right ? "Down" : "Up");
+		printf("1 %f %X\n", rightX, inRightX);
+		printf("1 %f %X\n", rightY, inRightY);
+		printf("2 %f %X\n", leftX, inLeftX);
+		printf("3 %f %X\n", leftY, inLeftY);
+		printf("A %s \n", left ? "Down" : "Up");
+		printf("B %s \n", right ? "Down" : "Up");
 		std::cout << "done\n";
 
 		unsigned long started_waiting_at = millis();
